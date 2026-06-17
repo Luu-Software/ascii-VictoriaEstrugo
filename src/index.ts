7ec8b0ac6603325dee3dcd9b1a5ed286fe77703e
VictoriaEstrugo <victoriaestrugo2011@gmail.com>
2026-06-17 16:01:56 -0300

    ASCII

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+let texto:string = " ";
+let contador:number = 0;
+
+while(contador < cantidad){
+texto = texto + letra;
+contador = contador + 1;
 }
+
+return texto;
+}
+
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+let contador:number=0;
+
+while(contador < tamaño){
+  console.log (repetirLetra(caracter,tamaño));
+  contador = contador + 1
+}
